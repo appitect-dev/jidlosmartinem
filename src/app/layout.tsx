0d3513e87e72d8e302dc4d0f2705a02596d06240
Jan Vandlíček <jan.vandlicek@gmail.com>
2025-09-05 10:44:22 +0200

    microsoft clarity init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import TabTitleChanger from "@/components/TabTitleChanger";
 
@@ -28,6 +29,17 @@ export default function RootLayout({
   return (
     <html lang="cs">
       <body className={`${inter.variable} font-sans antialiased`}>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t5x8g8y345");
+          `}
+        </Script>
+
         <TabTitleChanger />
         {children}
       </body>
